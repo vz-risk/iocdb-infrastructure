731a14755b4a554af48acc5ff097c47835617113
Nathan Buesgens <nathan@natb1.com>
2015-02-12 11:55:42 -0800

    elasticsearch recipe installs plugins

--- attributes/elasticsearch.rb
+++ attributes/elasticsearch.rb
@@ -1,2 +1,4 @@
 default[:elasticsearch][:version] = '1.4.1'
-default[:elasticsearch][:cluster][:name] = 'greenridge'
+default[:elasticsearch][:plugins] = {
+  'elasticsearch/elasticsearch-repository-hdfs/2.0.2' => {}
+}
--- recipes/elasticsearch.rb
+++ recipes/elasticsearch.rb
@@ -1,3 +1,7 @@
+node.default[:elasticsearch][:cluster][:name] = node[:envname]
+
 include_recipe 'iocdb-infrastructure::iocdb'
 include_recipe 'java'
 include_recipe 'elasticsearch'
+include_recipe 'elasticsearch::plugins'
+
