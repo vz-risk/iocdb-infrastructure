983b512c0517e6792c5ef8b6cf8bf73cfd40d325
Nathan Buesgens <nathan@natb1.com>
2015-02-20 14:11:52 -0500

    updates command line for celery

--- recipes/iocdb-worker.rb
+++ recipes/iocdb-worker.rb
@@ -14,12 +14,12 @@ end
 
 # TODO: I don't know why this command isn't working, just run manually
 #execute 'start workers' do
-  #command 'iocdb-worker multi start default --maxtasksperchild=1 -Q:default celery -l INFO --logfile=/var/log/iocdb-worker/%n.log --pidfile=/var/run/iocdb-worker/%n.pid'
+  #command 'celery -A dispatcher multi start default --maxtasksperchild=1 -Q:default celery -l INFO --logfile=/var/log/iocdb-worker/%n.log --pidfile=/var/run/iocdb-worker/%n.pid'
   #user 'iocdb_worker'
 #end
 
 # TODO: same as above
 #bash 'start beat' do
-#  command 'iocdb-worker beat -l INFO --logfile=/var/log/iocdb-worker/beat.log --pidfile=/var/run/iocdb-worker/beat.pid -s /var/run/iocdb-worker/beat-schedule --detach'
+#  command 'celery -A dispatcher beat -l INFO --logfile=/var/log/iocdb-worker/beat.log --pidfile=/var/run/iocdb-worker/beat.pid -s /var/run/iocdb-worker/beat-schedule --detach'
 #  user 'iocdb_worker'
 #end
