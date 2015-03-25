123ee1b2fe6f43a6824b16d77159d9a9b6b50db6
Kevin Graham <kevin.graham@verizon.com>
2015-03-25 15:05:50 +0000

    [IOCDB-16] Mods to supportchef provisioning from staged git clones rather than github.  New dev and qa environments cannot see github via ssh so the repo must first be cloned into the IDN

--- recipes/iocdb-client.rb
+++ recipes/iocdb-client.rb
@@ -5,25 +5,53 @@ include_recipe 'iocdb-infrastructure::iocdb'
 include_recipe 'git'
 include_recipe 'python'
 
-execute 'add github to known hosts' do
-  command 'ssh git@github.com -o StrictHostKeyChecking=no'
+#-------------------------------------------------------------------------------------------
+# New staged repos method
+#   Note: Use iocdb_prov and repos staged at ssh://iocdb_prov@iocdb-staging/staged-repos/ 
+#   now instead of nates old account and githib.
+#-------------------------------------------------------------------------------------------
+execute 'add host iocdb-staging to known hosts if not already there' do
+  command 'ssh iocdb_prov@iocdb-staging -o StrictHostKeyChecking=no'
   returns 1
 end
 
 python_pip 'mapping-tools' do
-  package_name 'git+https://github.com/natb1/mapping-tools.git#egg=mapping-tools'
+  package_name 'git+ssh://iocdb_prov@iocdb-staging/staged-repos/mapping-tools.git#egg=mapping-tools'
   options '-e'
 end
 
 python_pip 'query-tools' do
-  package_name 'git+https://github.com/natb1/query-tools.git#egg=query-tools'
+  package_name 'git+ssh://iocdb_prov@iocdb-staging/staged-repos/query-tools.git#egg=query-tools'
   options '-e'
 end
 
 python_pip 'iocdb' do
-  package_name 'git+git@github.com:vz-risk/iocdb.git#egg=iocdb'
+  package_name 'git+ssh://iocdb_prov@iocdb-staging/staged-repos/iocdb.git#egg=iocdb'
   options '-e'
 end
+
+#------------------------------------------------------------------------------------
+# Old github method
+#-------------------------------------------------------------------------------------------
+#execute 'add github to known hosts' do
+#  command 'ssh git@github.com -o StrictHostKeyChecking=no'
+#  returns 1
+#end
+#
+#python_pip 'mapping-tools' do
+#  package_name 'git+https://github.com/natb1/mapping-tools.git#egg=mapping-tools'
+#  options '-e'
+#end
+#
+#python_pip 'query-tools' do
+#  package_name 'git+https://github.com/natb1/query-tools.git#egg=query-tools'
+#  options '-e'
+#end
+#
+#python_pip 'iocdb' do
+#  package_name 'git+git@github.com:vz-risk/iocdb.git#egg=iocdb'
+#  options '-e'
+#end
 
 template '/src/iocdb/iocdb/data/iocdb_config.py' do
   source 'iocdb_config.erb'
