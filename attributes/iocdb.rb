d7e83579bc6b71cfe0deb92cf4677f6a961a1e59
Kevin Graham <kevin.graham@verizon.com>
2015-05-05 22:08:01 +0000

    [IOC-46] Mods to chef scripts to make configuration of iocdb cli easier

--- attributes/iocdb.rb
+++ attributes/iocdb.rb
@@ -1,2 +1,7 @@
+#default[:iocdb][:install][:branch] = origin/master
+default[:iocdb][:install][:branch] = origin/dev-ci
 default['set_fqdn'] = '*.vcic.local'
-default[:iocdb][:eshosts] = []
+default[:iocdb][:eshosts][:prod] = '[10.114.75.149, 10.114.75.152, 10.114.75.153]'
+default[:iocdb][:eshosts][:dev] = '[153.39.107.114]'
+default[:iocdb][:eshosts][:qa] = '[166.34.103.214]'
+
--- recipes/iocdb-client.rb
+++ recipes/iocdb-client.rb
@@ -5,54 +5,21 @@ include_recipe 'iocdb-infrastructure::iocdb'
 include_recipe 'git'
 include_recipe 'python'
 
-#-------------------------------------------------------------------------------------------
-# New staged repos method
-#   Note: Use iocdb_prov and repos staged at ssh://iocdb_prov@iocdb-staging/staged-repos/ 
-#   now instead of nates old account and githib.
-#-------------------------------------------------------------------------------------------
 execute 'add host iocdb-staging to known hosts if not already there' do
   command 'ssh iocdb_prov@iocdb-staging -o StrictHostKeyChecking=no'
   returns 0
 end
 
-python_pip 'mapping-tools' do
-  package_name 'git+ssh://iocdb_prov@iocdb-staging/staged-repos/mapping-tools.git#egg=mapping-tools'
-  options '-e'
-end
-
-python_pip 'query-tools' do
-  package_name 'git+ssh://iocdb_prov@iocdb-staging/staged-repos/query-tools.git#egg=query-tools'
-  options '-e'
-end
-
 python_pip 'iocdb' do
-  package_name 'git+ssh://iocdb_prov@iocdb-staging/staged-repos/iocdb.git#egg=iocdb'
+  package_name 'git+ssh://iocdb_prov@iocdb-staging/staged-repos/iocdb.git@dev-ci#egg=iocdb'
+#  package_name 'git+ssh://iocdb_prov@iocdb-staging/staged-repos/iocdb.git@origin/master#egg=iocdb'
   options '-e'
 end
 
-#------------------------------------------------------------------------------------
-# Old github method
-#-------------------------------------------------------------------------------------------
-#execute 'add github to known hosts' do
-#  command 'ssh git@github.com -o StrictHostKeyChecking=no'
-#  returns 1
-#end
-#
-#python_pip 'mapping-tools' do
-#  package_name 'git+https://github.com/natb1/mapping-tools.git#egg=mapping-tools'
-#  options '-e'
-#end
-#
-#python_pip 'query-tools' do
-#  package_name 'git+https://github.com/natb1/query-tools.git#egg=query-tools'
-#  options '-e'
-#end
-#
-#python_pip 'iocdb' do
-#  package_name 'git+git@github.com:vz-risk/iocdb.git#egg=iocdb'
-#  options '-e'
-#end
+template '/src/iocdb/iocdb/data/settings.yaml' do
+  source "host-#{node['hostname']}/settings.yaml"
+end
 
 template '/src/iocdb/iocdb/data/iocdb_config.py' do
-  source 'iocdb_config.erb'
+  source "host-#{node['hostname']}/iocdb_config.erb"
 end
