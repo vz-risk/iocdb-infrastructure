33f5806161c60780dff9ac3794c316a81aacd762
Kevin Graham <kevin.graham@verizon.com>
2015-05-05 22:39:39 +0000

    [IOC-46] attribute tweaks

--- attributes/iocdb.rb
+++ attributes/iocdb.rb
@@ -1,5 +1,3 @@
-#default[:iocdb][:install][:branch] = origin/master
-default[:iocdb][:install][:branch] = origin/dev-ci
 default['set_fqdn'] = '*.vcic.local'
 default[:iocdb][:eshosts][:prod] = '[10.114.75.149, 10.114.75.152, 10.114.75.153]'
 default[:iocdb][:eshosts][:dev] = '[153.39.107.114]'
