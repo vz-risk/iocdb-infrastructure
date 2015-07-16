89c99e3160b9c458206ff6c8aace68c2eaf2a28a
Kevin Graham <kevin.graham@verizon.com>
2015-07-16 09:23:40 -0600

    [IOC-141] Add init script for iocdb-rest command which now implements the data entry gui

--- recipes/iocdb-rest.rb
+++ recipes/iocdb-rest.rb
@@ -1,6 +1,16 @@
 include_recipe 'iocdb-infrastructure::iocdb-client'
-
 # iocdb-rest is now installed as part of the iocdb cli (iocdb-client). 
+
+execute 'stop iocdb-rest' do
+  command 'if [ -f /etc/init.d/iocdb-rest ]; then service iocdb-rest stop; fi'
+  user 'root'
+end
+
+# create log dir if needed
+directory '/var/log/iocdb' do
+  owner 'iocdb_prov'
+  mode '0775'
+end
 
 # add init script for iocdb-rest
 cookbook_file "iocdb-rest" do
